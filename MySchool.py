5f9c60d082d06a517820cee6fb8cf46c132f7bb1
Gietka Izabela <119822334+isagtk@users.noreply.github.com>
2023-01-09 22:19:57 +0100

    Update MySchool.py

--- MySchool.py
+++ MySchool.py
@@ -250,6 +250,7 @@ def main():
     
     
     st.sidebar.subheader('With Selection Generate Output')
+    st.sidebar.markdown(selection_name)
     time_str=time.strftime("%Y_%m_%d_%H%M")
     filename="Invoice_"+time_str + '_' + selection_name + '.xlsx'
 
