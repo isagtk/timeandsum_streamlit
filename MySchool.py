0c2e4b5a0e6823f391e11ab265f8e83c3f03307c
Gietka Izabela <119822334+isagtk@users.noreply.github.com>
2023-01-09 22:10:29 +0100

    Update MySchool.py

--- MySchool.py
+++ MySchool.py
@@ -252,16 +252,7 @@ def main():
     st.sidebar.subheader('With Selection Generate Output')
     time_str=time.strftime("%Y_%m_%d_%H%M")
     filename="Invoice_"+time_str + '_' + selection_name + '.xlsx'
-    
-    '''
-    if st.sidebar.button("Submit"):  
-        #writer=pd.ExcelWriter(filename, engine='xlsxwriter')
-        writer=pd.ExcelWriter(filename)
-        start_time=time.time()
-        print('\nExport of Excel have started.')
-        df1.to_excel(writer, sheet_name="DF", index=False)
-        writer.save()
-     '''
+
     buffer = io.BytesIO()    
     with pd.ExcelWriter(buffer, engine="xlsxwriter") as writer:
         df1.to_excel(writer, sheet_name=selection_name, index=False)
