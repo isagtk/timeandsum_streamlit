8044c771a9838b8f311540e5e8135aad39acd30c
Gietka Izabela <119822334+isagtk@users.noreply.github.com>
2023-01-12 22:39:07 +0100

    Update MySchool.py

--- MySchool.py
+++ MySchool.py
@@ -187,7 +187,7 @@ def main():
     
     if page=='URL':
            
-           uploaded_file=st.text_input(label = "Please enter tURL", default)          
+           uploaded_file=st.text_input(label = "Please enter URL", default='In progress')          
     else:
            uploaded_file = st.file_uploader("Data for Analysis <<MYDF1.xlsx>>", type='xlsx', accept_multiple_files=False, key=None, help=None, on_change=None, args=None, kwargs=None, disabled=False, label_visibility="visible")
     
