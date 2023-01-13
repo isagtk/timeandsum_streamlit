398bc1870b241b6b15b10739f5750a923f4fe021
Gietka Izabela <119822334+isagtk@users.noreply.github.com>
2023-01-13 22:11:53 +0100

    Update MySchool.py

--- MySchool.py
+++ MySchool.py
@@ -189,8 +189,8 @@ def main():
     if page=='URL - in progress !!!':
            url_file=st.text_input(label = "Please enter URL")
            st.write(url_file)
-           #urllib.request.urlretrieve("r'" + url_file +"'")
-           webbrowser.open_new_tab(url_file) 
+           webbrowser.open_new_tab(url_file)
+           uploaded_file =pd.read_excel() 
     else:
            uploaded_file = st.file_uploader("Data for Analysis <<MYDF1.xlsx>>", type='xlsx', accept_multiple_files=False, key=None, help=None, on_change=None, args=None, kwargs=None, disabled=False, label_visibility="visible")
     
